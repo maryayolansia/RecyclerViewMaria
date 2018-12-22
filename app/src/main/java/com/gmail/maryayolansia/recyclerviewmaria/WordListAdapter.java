30727166671cadffd44ac968ce86f598019cbfae
maryayolansia <maryayolansia@gmail.com>
2018-12-22 17:27:53 +0700

    test

---
+++ app/src/main/java/com/gmail/maryayolansia/recyclerviewmaria/WordListAdapter.java
@@ -0,0 +1,102 @@
+package com.gmail.maryayolansia.recyclerviewmaria;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+import static com.gmail.maryayolansia.recyclerviewmaria.R.string.clicked;
+
+/**
+ * Shows how to implement a simple Adapter for a RecyclerView.
+ * Demonstrates how to add a click handler for each item in the ViewHolder.
+ */
+public class WordListAdapter
+        extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
+
+    class WordViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+        /**
+         * Creates a new custom view holder to hold the view to display
+         * in the RecyclerView.
+         *
+         * @param itemView The view in which to display the data.
+         * @param adapter The adapter that manages the the data and
+         *                views for the RecyclerView.
+         */
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = (TextView) itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // Prepend "Clicked! " to the text in the view.
+            // Original code: wordItemView.setText("Clicked! "+ wordItemView
+            // .getText())
+            String clickOutput = v.getContext().getString(clicked) +
+                    wordItemView.getText();
+            wordItemView.setText(clickOutput);
+        }
+    }
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    /**
+     * Inflates an item view and returns a new view holder that contains it.
+     * Called when the RecyclerView needs a new view holder to represent an item.
+     *
+     * @param parent The view group that holds the item views.
+     * @param viewType Used to distinguish views, if more than one
+     *                 type of item view is used.
+     * @return a view holder.
+     */
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate an item view.
+        View itemView = mInflater.inflate(R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(itemView, this);
+    }
+
+    /**
+     * Sets the contents of an item at a given position in the RecyclerView.
+     *
+     * @param holder The view holder for that position in the RecyclerView.
+     * @param position The position of the item in the RecycerView.
+     */
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        // Retrieve the data for that position.
+        String current = mWordList.get(position);
+        // Add the data to the view holder.
+        holder.wordItemView.setText(current);
+    }
+
+    /**
+     * Returns the size of the container that holds the data.
+     *
+     * @return Size of the list of data.
+     */
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+}
+
+
